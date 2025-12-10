9daa7677e9ae4cd078f4577749c4611ac5b7511c
Hoàng Thái Bình <hoangbinhkg@hotmail.com>
2025-12-10 09:45:05 +0700

    New Upload

--- ZeroWeb/wwwroot/Zero/assets/Custom/models/dhtmlxModels.js
+++
@@ -1,6 +0,0 @@
-﻿class DhtmlxTable {
-    static get btnAdd() { return "btnAdd"; }
-    static get btnEdit() { return "btnEdit"; }
-    static get btnSave() { return "btnSave"; }
-    static get btnUpdate() { return "btnUpdate"; }
-}
--- ZeroWeb/wwwroot/Zero/assets/Custom/models/environment.js
+++
@@ -1,5 +0,0 @@
-﻿class Environment {
-    static get basePath() { return environment.basePath; }
-    static get isProduction() { return environment.isProduction; }
-    static get isMultipleLanguages() { return environment.isMultipleLanguages; }
-}
